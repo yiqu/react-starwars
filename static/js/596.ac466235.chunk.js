8d760a6aa8e6171ce33a04e838cc47b76bc2f6a8
CircleCi <circleci@example.com>
2022-11-14 04:02:01 +0000

    [ci skip]

---
+++ static/js/596.ac466235.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstarwars=self.webpackChunkstarwars||[]).push([[596],{1596:function(r,s,t){t.r(s);var u=t(184);s.default=function(){return(0,u.jsx)("div",{children:"About"})}}}]);
+//# sourceMappingURL=596.ac466235.chunk.js.map
---
+++ static/js/784.407ccf0d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstarwars=self.webpackChunkstarwars||[]).push([[784],{9784:function(n,r,s){s.r(r);var a=s(184);r.default=function(){return(0,a.jsx)("div",{children:"Page not found!"})}}}]);
+//# sourceMappingURL=784.407ccf0d.chunk.js.map
