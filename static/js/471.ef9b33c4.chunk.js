16a105a8834830af9f413b53681f10664f4b6eb1
CircleCi <circleci@example.com>
2023-03-05 04:30:36 +0000

    [ci skip]

---
+++ static/js/471.ef9b33c4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstarwars=self.webpackChunkstarwars||[]).push([[471],{4471:function(s,r,t){t.r(r);var u=t(72791),a=t(57689),e=t(64317),n=t(61022),f=t(80184);r.default=function(){var s=(0,e.T)();return(0,u.useEffect)((function(){s((0,n.E)())}),[s]),(0,f.jsx)(a.j3,{})}}}]);
+//# sourceMappingURL=471.ef9b33c4.chunk.js.map
