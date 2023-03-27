dc5306024ce3bf428cd3edac91b198d26cac0c63
CircleCi <circleci@example.com>
2023-03-27 16:26:12 +0000

    [ci skip]

---
+++ assets/About-98fa2a24.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-e4a30620.js";const r=()=>t("div",{children:"About"});export{r as default};
---
+++ assets/CoreEntityDetail-92246019.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-e4a30620.js";function i(){return t("div",{children:"Detail"})}export{i as default};
